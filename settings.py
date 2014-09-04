7eb8957c8912f1ba7fc3c24ca5c1cbf96a898ee8
JeanAbbiateci <j.abbiateci@gmail.com>
2014-09-04 19:52:07 +0200

    Update settings.py
    add

--- settings.py
+++ settings.py
@@ -49,11 +49,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = 'Europe/Zurich'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-CH'
 
 SITE_ID = 1
 
